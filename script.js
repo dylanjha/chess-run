e1a751fa31ea42821376cc0a5fefd337cc07d1fe
Dylan Jhaveri <dylanjhaveri@gmail.com>
2012-07-22 20:02:49 -0700

    countdown on load, puase on click

--- script.js
+++ script.js
@@ -16,32 +16,37 @@ $(function() {
     });
 
   });
-  // $('#black').click(function() 
-    //   function pauseBlack() {
-    //     $('#black').countdown.stop();
-    //   }
 
-    //   function startBlack() {    
-    //     $('#black').countdown({
-    //       until: count,
-    //       format: 'MS',
-    //       layout: '{mn}'+':'+'{sn}'
-    //     });
-    //   }
-    // }); 
+  $('#black').click(function() { 
+    // function pauseBlack() {
+      console.log('pauseblack');
+      $('#black').countdown('pause');
+    // }
+  });
 
-    // $('#white').click(function()
-    //   function pauseWhite() {
-    //     $('#white').countdown.stop();
-    //   }
+  //     function startBlack() {    
+  //       $('#black').countdown({
+  //         until: count,
+  //         format: 'MS',
+  //         layout: '{mn}'+':'+'{sn}'
+  //       });
+  //     }
+  //   }); 
 
-    //   function startWhite() {
-    //     $('#white').countdown({
-    //       until: count
-    //       format: 'MS',
-    //       layout: '{mn}'+':'+'{sn}'
-    //     });
-    //   }
-    // });
-  
+  $('#white').click(function() {
+    // function pauseWhite() {
+      console.log('pausewhite');
+      $('#white').countdown('pause');
+    // }
+  });
+
+  //     function startWhite() {
+  //       $('#white').countdown({
+  //         until: count
+  //         format: 'MS',
+  //         layout: '{mn}'+':'+'{sn}'
+  //       });
+  //     }
+  //   });
+
 });
